337bb8ef00d03eef86823f29aeaa83d2187bb4e6
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:52:47 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,27 +1,72 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Reptil extends Animal {
-    private static int cantidadReptiles;
+    private String colorEscamas;
+    private int largoCola;
+    public static int iguanas = 0;
+    public static int serpientes = 0;
+    private static List<Reptil> listado = new ArrayList<>();
 
     public Reptil() {
-        cantidadReptiles++;
+        super();
+        listado.add(this);
     }
 
-    public Reptil(String nombre, int edad, String habitat, String genero) {
+    public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
         super(nombre, edad, habitat, genero);
-        cantidadReptiles++;
+        this.colorEscamas = colorEscamas;
+        this.largoCola = largoCola;
+        listado.add(this);
     }
-
     @Override
     public String movimiento() {
         return "reptar";
     }
 
-    public static int cantidadReptiles() {
-        return cantidadReptiles;
+    public static Reptil crearIguana(String nombre, int edad, String genero) {
+        iguanas++;
+        Reptil iguana = new Reptil(nombre, edad, "humedal", genero, "verde", 3);
+        listado.add(iguana);
+        return iguana;
     }
 
-    public static void setCantidadReptiles(int cantidadReptiles) {
-        Reptil.cantidadReptiles = cantidadReptiles;
+    public static Reptil crearSerpiente(String nombre, int edad, String genero) {
+        serpientes++;
+        Reptil serpiente = new Reptil(nombre, edad, "jungla", genero, "marron", 2);
+        listado.add(serpiente);
+        return serpiente;
+    }
+
+    public static int cantidadReptiles() {
+        return listado.size();
+    }
+
+    public static List<Reptil> getListado() {
+        return listado;
+    }
+
+    public static void setListado(List<Reptil> listado) {
+        Reptil.listado = listado;
+    }
+
+    public String getColorEscamas() {
+        return colorEscamas;
+    }
+
+    public void setColorEscamas(String colorEscamas) {
+        this.colorEscamas = colorEscamas;
+    }
+
+    public int getLargoCola() {
+        return largoCola;
+    }
+
+    public void setLargoCola(int largoCola) {
+        this.largoCola = largoCola;
     }
 }
+
+  

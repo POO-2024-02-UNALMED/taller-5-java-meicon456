ac523d1ba009cb2f13bf99754fd8b886249d3b53
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:17:41 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,52 +1,37 @@
 package zooAnimales;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Pez extends Animal {
     private String colorEscamas;
     private int cantidadAletas;
+
     public static int salmones = 0;
     public static int bacalaos = 0;
-    private static List<Pez> listado = new ArrayList<>();
 
     public Pez() {
         super();
-        listado.add(this);
     }
 
     public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
         super(nombre, edad, habitat, genero);
         this.colorEscamas = colorEscamas;
         this.cantidadAletas = cantidadAletas;
-        listado.add(this);
+    }
+
+    @Override
+    public String movimiento() {
+        return "nadar";
     }
 
     public static Pez crearSalmon(String nombre, int edad, String genero) {
         salmones++;
-        Pez salmon = new Pez(nombre, edad, "oceano", genero, "rojo", 6);
-        listado.add(salmon);
-        return salmon;
+        return new Pez(nombre, edad, "oceano", genero, "rojo", 4);
     }
 
     public static Pez crearBacalao(String nombre, int edad, String genero) {
         bacalaos++;
-        Pez bacalao = new Pez(nombre, edad, "oceano", genero, "gris", 6);
-        listado.add(bacalao);
-        return bacalao;
+        return new Pez(nombre, edad, "oceano", genero, "gris", 2);
     }
 
-    public static int cantidadPeces() {
-        return listado.size();
-    }
-
-    public static List<Pez> getListado() {
-        return listado;
-    }
-
-    public static void setListado(List<Pez> listado) {
-        Pez.listado = listado;
-    }
 
     public String getColorEscamas() {
         return colorEscamas;

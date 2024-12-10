138cb9b6480bfcad3626bf3cb709e65d7690dd0f
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:46:26 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,57 +1,27 @@
 package zooAnimales;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Ave extends Animal {
-    private String colorPlumas;
-    public static int aguilas = 0;
-    public static int halcones = 0;
-    private static List<Ave> listado = new ArrayList<>();
+    private static int cantidadAves;
 
     public Ave() {
-        super();
-        listado.add(this);
+        cantidadAves++;
     }
 
-    public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
+    public Ave(String nombre, int edad, String habitat, String genero) {
         super(nombre, edad, habitat, genero);
-        this.colorPlumas = colorPlumas;
-        listado.add(this);
+        cantidadAves++;
     }
 
-    public static Ave crearAguila(String nombre, int edad, String genero) {
-        aguilas++;
-        Ave aguila = new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
-        listado.add(aguila);
-        return aguila;
-    }
-
-    public static Ave crearHalcon(String nombre, int edad, String genero) {
-        halcones++;
-        Ave halcon = new Ave(nombre, edad, "ciudad", genero, "gris");
-        listado.add(halcon);
-        return halcon;
+    @Override
+    public String movimiento() {
+        return "volar";
     }
 
     public static int cantidadAves() {
-        return listado.size();
+        return cantidadAves;
     }
 
-    public static List<Ave> getListado() {
-        return listado;
-    }
-
-    public static void setListado(List<Ave> listado) {
-        Ave.listado = listado;
-    }
-
-
-    public String getColorPlumas() {
-        return colorPlumas;
-    }
-
-    public void setColorPlumas(String colorPlumas) {
-        this.colorPlumas = colorPlumas;
+    public static void setCantidadAves(int cantidadAves) {
+        Ave.cantidadAves = cantidadAves;
     }
 }

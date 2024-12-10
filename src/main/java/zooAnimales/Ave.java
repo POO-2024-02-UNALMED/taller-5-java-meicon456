12b0ee45f483f76cc3f29ed33e803471db0ca0d2
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:37:28 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -1,33 +1,49 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ave extends Animal {
     private String colorPlumas;
-
-    public static int halcones = 0;
     public static int aguilas = 0;
+    public static int halcones = 0;
+    private static List<Ave> listado = new ArrayList<>();
 
     public Ave() {
         super();
+        listado.add(this);
     }
 
     public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
         super(nombre, edad, habitat, genero);
         this.colorPlumas = colorPlumas;
-    }
-
-    @Override
-    public String movimiento() {
-        return "volar";
-    }
-
-    public static Ave crearHalcon(String nombre, int edad, String genero) {
-        halcones++;
-        return new Ave(nombre, edad, "montanas", genero, "cafe glorioso");
+        listado.add(this);
     }
 
     public static Ave crearAguila(String nombre, int edad, String genero) {
         aguilas++;
-        return new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
+        Ave aguila = new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
+        listado.add(aguila);
+        return aguila;
+    }
+
+    public static Ave crearHalcon(String nombre, int edad, String genero) {
+        halcones++;
+        Ave halcon = new Ave(nombre, edad, "ciudad", genero, "gris");
+        listado.add(halcon);
+        return halcon;
+    }
+
+    public static int cantidadAves() {
+        return listado.size();
+    }
+
+    public static List<Ave> getListado() {
+        return listado;
+    }
+
+    public static void setListado(List<Ave> listado) {
+        Ave.listado = listado;
     }
 
 

f0b7fdb649159746c2a605eac305096fe24e6b11
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:53:17 -0500

    Update Pez.java

--- src/main/java/zooAnimales/Pez.java
+++ src/main/java/zooAnimales/Pez.java
@@ -1,27 +1,73 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Pez extends Animal {
-    private static int cantidadPeces;
+    private String colorEscamas;
+    private int cantidadAletas;
+    public static int salmones = 0;
+    public static int bacalaos = 0;
+    private static List<Pez> listado = new ArrayList<>();
 
     public Pez() {
-        cantidadPeces++;
+        super();
+        listado.add(this);
     }
 
-    public Pez(String nombre, int edad, String habitat, String genero) {
+    public Pez(String nombre, int edad, String habitat, String genero, String colorEscamas, int cantidadAletas) {
         super(nombre, edad, habitat, genero);
-        cantidadPeces++;
+        this.colorEscamas = colorEscamas;
+        this.cantidadAletas = cantidadAletas;
+        listado.add(this);
     }
-
     @Override
     public String movimiento() {
         return "nadar";
     }
 
-    public static int cantidadPeces() {
-        return cantidadPeces;
+
+    public static Pez crearSalmon(String nombre, int edad, String genero) {
+        salmones++;
+        Pez salmon = new Pez(nombre, edad, "oceano", genero, "rojo", 6);
+        listado.add(salmon);
+        return salmon;
     }
 
-    public static void setCantidadPeces(int cantidadPeces) {
-        Pez.cantidadPeces = cantidadPeces;
+    public static Pez crearBacalao(String nombre, int edad, String genero) {
+        bacalaos++;
+        Pez bacalao = new Pez(nombre, edad, "oceano", genero, "gris", 6);
+        listado.add(bacalao);
+        return bacalao;
+    }
+
+    public static int cantidadPeces() {
+        return listado.size();
+    }
+
+    public static List<Pez> getListado() {
+        return listado;
+    }
+
+    public static void setListado(List<Pez> listado) {
+        Pez.listado = listado;
+    }
+
+
+    public String getColorEscamas() {
+        return colorEscamas;
+    }
+
+    public void setColorEscamas(String colorEscamas) {
+        this.colorEscamas = colorEscamas;
+    }
+
+    public int getCantidadAletas() {
+        return cantidadAletas;
+    }
+
+    public void setCantidadAletas(int cantidadAletas) {
+        this.cantidadAletas = cantidadAletas;
     }
 }
+

602cf07029ffe64042858bd35e0b8a2a6bff6cc7
meicon456 <jocastrore@unal.edu.co>
2024-12-09 21:38:19 -0500

    Update Zoologico.java

--- src/main/java/gestion/Zoologico.java
+++ src/main/java/gestion/Zoologico.java
@@ -1,53 +1,24 @@
 package gestion;
 
-import java.util.ArrayList;
 import zooAnimales.Animal;
+import java.util.List;
 
 public class Zoologico {
-    private String nombre;
-    private String ubicacion;
-    private ArrayList<Zona> zonas = new ArrayList<>();
+    private List<Zona> zonas;
 
-    public Zoologico() {}
-
-    public Zoologico(String nombre, String ubicacion) {
-        this.nombre = nombre;
-        this.ubicacion = ubicacion;
+    public Zoologico(List<Zona> zonas) {
+        this.zonas = zonas;
     }
 
-    public void agregarZonas(Zona zona) {
-        zonas.add(zona);
-    }
-
-    public int cantidadTotalAnimales() {
-        int total = 0;
-        for (Zona zona : zonas) {
-            total += zona.cantidadAnimales();
-        }
-        return total;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getUbicacion() {
-        return ubicacion;
-    }
-
-    public void setUbicacion(String ubicacion) {
-        this.ubicacion = ubicacion;
-    }
-
-    public ArrayList<Zona> getZonas() {
+    public List<Zona> getZonas() {
         return zonas;
     }
 
-    public void setZonas(ArrayList<Zona> zonas) {
-        this.zonas = zonas;
+    public void mostrarAnimales() {
+        for (Zona zona : zonas) {
+            for (Animal animal : zona.getAnimales()) {
+                System.out.println(animal);
+            }
+        }
     }
 }

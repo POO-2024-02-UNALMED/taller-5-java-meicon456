9d64324fdca6138a451a5a08ec49ce4f76eb98ce
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:16:16 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -4,6 +4,9 @@ public class Mamifero extends Animal {
     private boolean pelaje;
     private int patas;
 
+    public static int caballos = 0;
+    public static int leones = 0;
+
     public Mamifero() {
         super();
     }
@@ -19,5 +22,30 @@ public class Mamifero extends Animal {
         return "caminar";
     }
 
-    // Getters y setters...
+    public static Mamifero crearCaballo(String nombre, int edad, String genero) {
+        caballos++;
+        return new Mamifero(nombre, edad, "pradera", genero, true, 4);
+    }
+
+    public static Mamifero crearLeon(String nombre, int edad, String genero) {
+        leones++;
+        return new Mamifero(nombre, edad, "selva", genero, true, 4);
+    }
+
+
+    public boolean isPelaje() {
+        return pelaje;
+    }
+
+    public void setPelaje(boolean pelaje) {
+        this.pelaje = pelaje;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
+    }
 }

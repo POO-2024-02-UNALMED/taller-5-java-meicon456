ed9d4611a55d5210858e9267a32ff5511244eeac
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:50:38 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,27 +1,72 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mamifero extends Animal {
-    private static int cantidadMamiferos;
+    private boolean pelaje;
+    private int patas;
+    public static int caballos = 0;
+    public static int leones = 0;
+    private static List<Mamifero> listado = new ArrayList<>();
 
     public Mamifero() {
-        cantidadMamiferos++;
+        super();
+        listado.add(this);
     }
 
-    public Mamifero(String nombre, int edad, String habitat, String genero) {
+    public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
         super(nombre, edad, habitat, genero);
-        cantidadMamiferos++;
+        this.pelaje = pelaje;
+        this.patas = patas;
+        listado.add(this);
     }
 
+    public static Mamifero crearCaballo(String nombre, int edad, String genero) {
+        caballos++;
+        Mamifero caballo = new Mamifero(nombre, edad, "pradera", genero, true, 4);
+        listado.add(caballo);
+        return caballo;
+    }
+
+    public static Mamifero crearLeon(String nombre, int edad, String genero) {
+        leones++;
+        Mamifero leon = new Mamifero(nombre, edad, "selva", genero, true, 4);
+        listado.add(leon);
+        return leon;
+    }
+
+    public static int cantidadMamiferos() {
+        return listado.size();
+    }
     @Override
     public String movimiento() {
         return "desplazarse";
     }
 
-    public static int cantidadMamiferos() {
-        return cantidadMamiferos;
+    public static List<Mamifero> getListado() {
+        return listado;
     }
 
-    public static void setCantidadMamiferos(int cantidadMamiferos) {
-        Mamifero.cantidadMamiferos = cantidadMamiferos;
+    public static void setListado(List<Mamifero> listado) {
+        Mamifero.listado = listado;
+    }
+
+    public boolean isPelaje() {
+        return pelaje;
+    }
+
+    public void setPelaje(boolean pelaje) {
+        this.pelaje = pelaje;
+    }
+
+    public int getPatas() {
+        return patas;
+    }
+
+    public void setPatas(int patas) {
+        this.patas = patas;
     }
 }
+
+   

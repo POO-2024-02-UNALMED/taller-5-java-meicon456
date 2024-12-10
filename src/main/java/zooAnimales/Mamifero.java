b4ec815d31456c5e22986fec1f95e82182c1faeb
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:37:45 -0500

    Update Mamifero.java

--- src/main/java/zooAnimales/Mamifero.java
+++ src/main/java/zooAnimales/Mamifero.java
@@ -1,37 +1,52 @@
 package zooAnimales;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mamifero extends Animal {
     private boolean pelaje;
     private int patas;
-
     public static int caballos = 0;
     public static int leones = 0;
+    private static List<Mamifero> listado = new ArrayList<>();
 
     public Mamifero() {
         super();
+        listado.add(this);
     }
 
     public Mamifero(String nombre, int edad, String habitat, String genero, boolean pelaje, int patas) {
         super(nombre, edad, habitat, genero);
         this.pelaje = pelaje;
         this.patas = patas;
-    }
-
-    @Override
-    public String movimiento() {
-        return "caminar";
+        listado.add(this);
     }
 
     public static Mamifero crearCaballo(String nombre, int edad, String genero) {
         caballos++;
-        return new Mamifero(nombre, edad, "pradera", genero, true, 4);
+        Mamifero caballo = new Mamifero(nombre, edad, "pradera", genero, true, 4);
+        listado.add(caballo);
+        return caballo;
     }
 
     public static Mamifero crearLeon(String nombre, int edad, String genero) {
         leones++;
-        return new Mamifero(nombre, edad, "selva", genero, true, 4);
+        Mamifero leon = new Mamifero(nombre, edad, "selva", genero, true, 4);
+        listado.add(leon);
+        return leon;
     }
 
+    public static int cantidadMamiferos() {
+        return listado.size();
+    }
+
+    public static List<Mamifero> getListado() {
+        return listado;
+    }
+
+    public static void setListado(List<Mamifero> listado) {
+        Mamifero.listado = listado;
+    }
 
     public boolean isPelaje() {
         return pelaje;

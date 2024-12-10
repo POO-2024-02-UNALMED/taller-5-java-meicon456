fb0bda44fab74cf4e94f5e28b9fc7422192cb228
meicon456 <jocastrore@unal.edu.co>
2024-12-09 21:29:13 -0500

    Create Anfibio.java

---
+++ src/main/java/zooAnimales/Anfibio.java
@@ -0,0 +1,59 @@
+package zooAnimales;
+
+import java.util.ArrayList;
+
+public class Anfibio extends Animal {
+    private static ArrayList<Anfibio> listado = new ArrayList<>();
+    private static int ranas;
+    private static int salamandras;
+    private String colorPiel;
+    private boolean venenoso;
+
+    public Anfibio() {
+        listado.add(this);
+    }
+
+    public Anfibio(String nombre, int edad, String habitat, String genero, String colorPiel, boolean venenoso) {
+        super(nombre, edad, habitat, genero);
+        this.colorPiel = colorPiel;
+        this.venenoso = venenoso;
+        listado.add(this);
+    }
+
+    public static Anfibio crearRana(String nombre, int edad, String genero) {
+        Anfibio rana = new Anfibio(nombre, edad, "selva", genero, "verde", true);
+        ranas++;
+        return rana;
+    }
+
+    public static Anfibio crearSalamandra(String nombre, int edad, String genero) {
+        Anfibio salamandra = new Anfibio(nombre, edad, "selva", genero, "negro y amarillo", false);
+        salamandras++;
+        return salamandra;
+    }
+
+    public static int cantidadAnfibios() {
+        return listado.size();
+    }
+
+    @Override
+    public String movimiento() {
+        return "saltar";
+    }
+
+    public String getColorPiel() {
+        return colorPiel;
+    }
+
+    public void setColorPiel(String colorPiel) {
+        this.colorPiel = colorPiel;
+    }
+
+    public boolean isVenenoso() {
+        return venenoso;
+    }
+
+    public void setVenenoso(boolean venenoso) {
+        this.venenoso = venenoso;
+    }
+}

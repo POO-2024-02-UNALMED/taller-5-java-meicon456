326489909692dcf1b45f1e759d391be12484e9ac
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:47:12 -0500

    Update Reptil.java

--- src/main/java/zooAnimales/Reptil.java
+++ src/main/java/zooAnimales/Reptil.java
@@ -1,67 +1,27 @@
 package zooAnimales;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class Reptil extends Animal {
-    private String colorEscamas;
-    private int largoCola;
-    public static int iguanas = 0;
-    public static int serpientes = 0;
-    private static List<Reptil> listado = new ArrayList<>();
+    private static int cantidadReptiles;
 
     public Reptil() {
-        super();
-        listado.add(this);
+        cantidadReptiles++;
     }
 
-    public Reptil(String nombre, int edad, String habitat, String genero, String colorEscamas, int largoCola) {
+    public Reptil(String nombre, int edad, String habitat, String genero) {
         super(nombre, edad, habitat, genero);
-        this.colorEscamas = colorEscamas;
-        this.largoCola = largoCola;
-        listado.add(this);
+        cantidadReptiles++;
     }
 
-    public static Reptil crearIguana(String nombre, int edad, String genero) {
-        iguanas++;
-        Reptil iguana = new Reptil(nombre, edad, "humedal", genero, "verde", 3);
-        listado.add(iguana);
-        return iguana;
-    }
-
-    public static Reptil crearSerpiente(String nombre, int edad, String genero) {
-        serpientes++;
-        Reptil serpiente = new Reptil(nombre, edad, "jungla", genero, "marron", 2);
-        listado.add(serpiente);
-        return serpiente;
+    @Override
+    public String movimiento() {
+        return "reptar";
     }
 
     public static int cantidadReptiles() {
-        return listado.size();
+        return cantidadReptiles;
     }
 
-    public static List<Reptil> getListado() {
-        return listado;
-    }
-
-    public static void setListado(List<Reptil> listado) {
-        Reptil.listado = listado;
-    }
-
-
-    public String getColorEscamas() {
-        return colorEscamas;
-    }
-
-    public void setColorEscamas(String colorEscamas) {
-        this.colorEscamas = colorEscamas;
-    }
-
-    public int getLargoCola() {
-        return largoCola;
-    }
-
-    public void setLargoCola(int largoCola) {
-        this.largoCola = largoCola;
+    public static void setCantidadReptiles(int cantidadReptiles) {
+        Reptil.cantidadReptiles = cantidadReptiles;
     }
 }

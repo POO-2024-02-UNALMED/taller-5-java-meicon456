11334414df70d6063b46a849773e57f46585406f
meicon456 <jocastrore@unal.edu.co>
2024-12-09 21:44:36 -0500

    Update Ave.java

--- src/main/java/zooAnimales/Ave.java
+++ src/main/java/zooAnimales/Ave.java
@@ -4,42 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Ave extends Animal {
-    private static int halcones;
-    private static int aguilas;
-    private static List<Ave> aves = new ArrayList<>();
     private String colorPlumas;
+    public static int aguilas = 0;
+    public static int halcones = 0;
+    private static List<Ave> listado = new ArrayList<>();
+
+    public Ave() {
+        super();
+        listado.add(this);
+    }
 
     public Ave(String nombre, int edad, String habitat, String genero, String colorPlumas) {
         super(nombre, edad, habitat, genero);
         this.colorPlumas = colorPlumas;
-    }
-
-    public static Ave crearHalcon(String nombre, int edad, String genero) {
-        halcones++;
-        Ave halcon = new Ave(nombre, edad, "montanas", genero, "cafe glorioso");
-        aves.add(halcon);
-        return halcon;
+        listado.add(this);
     }
 
     public static Ave crearAguila(String nombre, int edad, String genero) {
         aguilas++;
         Ave aguila = new Ave(nombre, edad, "montanas", genero, "blanco y amarillo");
-        aves.add(aguila);
+        listado.add(aguila);
         return aguila;
     }
 
-    public static int getHalcones() {
-        return halcones;
+    public static Ave crearHalcon(String nombre, int edad, String genero) {
+        halcones++;
+        Ave halcon = new Ave(nombre, edad, "ciudad", genero, "gris");
+        listado.add(halcon);
+        return halcon;
     }
 
-    public static int getAguilas() {
-        return aguilas;
+    public static int cantidadAves() {
+        return listado.size();
     }
 
-    public static void mostrarAves() {
-        System.out.println("Listado de Aves:");
-        for (Ave ave : aves) {
-            System.out.println("Nombre: " + ave.nombre + ", Edad: " + ave.edad + " años, Genero: " + ave.genero);
-        }
+    public static List<Ave> getListado() {
+        return listado;
+    }
+
+    public static void setListado(List<Ave> listado) {
+        Ave.listado = listado;
+    }
+
+
+    public String getColorPlumas() {
+        return colorPlumas;
+    }
+
+    public void setColorPlumas(String colorPlumas) {
+        this.colorPlumas = colorPlumas;
     }
 }

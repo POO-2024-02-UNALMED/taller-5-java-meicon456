373833b3e1e12a7bf091f4d627024d9e842c32af
meicon456 <jocastrore@unal.edu.co>
2024-12-09 22:39:06 -0500

    Update Animal.java

--- src/main/java/zooAnimales/Animal.java
+++ src/main/java/zooAnimales/Animal.java
@@ -5,21 +5,41 @@ public class Animal {
     private int edad;
     private String habitat;
     private String genero;
+    private static int totalAnimales = 0;
 
-    public Animal() {}
+    public Animal() {
+        totalAnimales++;
+    }
 
     public Animal(String nombre, int edad, String habitat, String genero) {
         this.nombre = nombre;
         this.edad = edad;
         this.habitat = habitat;
         this.genero = genero;
+        totalAnimales++;
     }
 
     public String movimiento() {
         return "desplazarse";
     }
 
+    public static String totalPorTipo() {
+        return "Mamiferos: " + Mamifero.cantidadMamiferos() + "\n" +
+               "Aves: " + Ave.cantidadAves() + "\n" +
+               "Reptiles: " + Reptil.cantidadReptiles() + "\n" +
+               "Peces: " + Pez.cantidadPeces() + "\n" +
+               "Anfibios: " + Anfibio.cantidadAnfibios();
+    }
 
+    @Override
+    public String toString() {
+        return "Mi nombre es " + nombre + ", tengo una edad de " + edad + 
+               ", habito en " + habitat + " y mi genero es " + genero;
+    }
+
+    public static int getTotalAnimales() {
+        return totalAnimales;
+    }
     public String getNombre() {
         return nombre;
     }
@@ -52,3 +72,4 @@ public class Animal {
         this.genero = genero;
     }
 }
+

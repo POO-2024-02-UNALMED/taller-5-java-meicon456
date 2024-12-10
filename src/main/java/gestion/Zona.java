fd71daa831d53b5c9c5d7f1193da83264b44f6f0
meicon456 <jocastrore@unal.edu.co>
2024-12-09 21:46:52 -0500

    Update Zona.java

--- src/main/java/gestion/Zona.java
+++ src/main/java/gestion/Zona.java
@@ -1,12 +1,13 @@
 package gestion;
 
 import java.util.ArrayList;
+import java.util.List;
 import zooAnimales.Animal;
 
 public class Zona {
     private String nombre;
     private Zoologico zoo;
-    private ArrayList<Animal> animales = new ArrayList<>();
+    private List<Animal> animales = new ArrayList<>();
 
     public Zona() {}
 
@@ -38,12 +39,5 @@ public class Zona {
     public void setZoo(Zoologico zoo) {
         this.zoo = zoo;
     }
-
-    public ArrayList<Animal> getAnimales() {
-        return animales;
-    }
-
-    public void setAnimales(ArrayList<Animal> animales) {
-        this.animales = animales;
-    }
 }
+
